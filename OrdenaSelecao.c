f10e7e07d3276e54f082b283c06f8ee39a9aa191
vitorgregorio1998 <vitorgregorio@alunos.utfpr.edu.br>
2021-03-22 16:03:58 -0300

    Segundo commit

--- OrdenaSelecao.c
+++ OrdenaSelecao.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
+#include <time.h>
 
 int main(){
 
-    printf("hello");
+    int vetor = [6 4 2 7 5 1 0 8 9 3];
+    clock_t Ti, Tf;
+
+    Ti = clock();
+
+    
+
+    Tf = clock();
 
 }
